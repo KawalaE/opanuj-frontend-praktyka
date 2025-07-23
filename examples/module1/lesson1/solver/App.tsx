4ba6661c5dab7b2ebac9f83a41b39dcd80f99ea0
KawalaE <edyta.wer.kawala@gmail.com>
2025-07-23 18:29:49 +0200

    soultion to lesson1 solver

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
-import { f1, f2, f3, f4 } from './functions';
+import { operations } from './functions';
 
 const App = () => {
   const [numA, setNumA] = useState<number>(0);
   const [numB, setNumB] = useState<number>(0);
-  const [numC, setNumC] = useState<number | string>(0);
-
-  const doWork = (func: (a: number, b: number) => number) => {
-    setNumC(func(numA, numB));
-  };
+  const [result, setResult] = useState<number | string>(0);
 
   return (
     <div>
@@ -17,42 +13,31 @@ const App = () => {
           type="number"
           className="rounded-md shadow-md p-4"
           value={numA}
-          onChange={(e) => setNumA(parseFloat(e.target.value))}
+          onChange={(e) =>
+            setNumA(e.target.value === '' ? 0 : parseFloat(e.target.value))
+          }
         />
         <input
           type="number"
           className="rounded-md shadow-md p-4"
           value={numB}
-          onChange={(e) => setNumB(parseFloat(e.target.value))}
+          onChange={(e) =>
+            setNumB(e.target.value === '' ? 0 : parseFloat(e.target.value))
+          }
         />
       </div>
       <div className="grid grid-cols-4 gap-x-4 my-4">
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f1)}
-        >
-          +
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f2)}
-        >
-          -
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f3)}
-        >
-          *
-        </button>
-        <button
-          className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-          onClick={() => doWork(f4)}
-        >
-          /
-        </button>
+        {operations.map((operation) => (
+          <button
+            key={operation.sign}
+            className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
+            onClick={() => setResult(operation.method(numA, numB))}
+          >
+            {operation.sign}
+          </button>
+        ))}
       </div>
-      <div>Result: {numC}</div>
+      <div>Result: {result}</div>
     </div>
   );
 };
